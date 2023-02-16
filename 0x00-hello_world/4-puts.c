1eb83e73c6e07f907191266692b255ab150d1f7c
Wambui-Kiniiya <wambui.kiela@gmail.com>
2023-02-16 07:12:58 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- * The main function where it allows us to write a program
- *
- * Return always returns value (0)
+ * The main - function where it allows us to write a program
+ * Return: always returns value (0)
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
