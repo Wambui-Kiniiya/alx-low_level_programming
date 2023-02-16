6175ffefc841ffe46df0c458d4e4711a94866c8a
Wambui-Kiniiya <wambui.kiela@gmail.com>
2023-02-16 07:38:32 -0800

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point where the code goes using printf function
+ * Return: always returns value (0)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
