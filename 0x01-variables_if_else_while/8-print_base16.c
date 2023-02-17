102858f49cb15be5071549634c048b700edeb530
Wambui-Kiniiya <wambui.kiela@gmail.com>
2023-02-17 10:53:23 -0800

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all numbers of base 16
+ * in lowercase
+ * Return: value (0)
+ */
+int main(void)
+{
+	int num;
+	char ch;
+
+	for (num = 0; nm < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
