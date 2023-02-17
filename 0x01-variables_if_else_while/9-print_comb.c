8c6b87c7feab445e930196f932487714fa1bb6fb
Wambui-Kiniiya <wambui.kiela@gmail.com>
2023-02-17 11:06:38 -0800

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations
+ * of single-digits
+ * Return: value (0)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 20; n < 30; n++)
+	{
+		putchar(n);
+		if (n != 29)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
+	}
+	return (0);
+}
