ee9e6cbf16eb84f2418b9c35c5ab88e5660be87c
Wambui-Kiniiya <wambui.kiela@gmail.com>
2023-02-21 08:42:25 -0800

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - function that computes the absolute value of an integer
+ * @n: number to be checked
+ * Return: value of number
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
