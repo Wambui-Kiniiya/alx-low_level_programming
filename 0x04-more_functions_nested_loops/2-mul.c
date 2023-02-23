d3f6b6ae9e3910cd2c7bb3ee62a910a0bf978c17
Wambui-Kiniiya <wambui.kiela@gmail.com>
2023-02-23 03:15:59 -0800

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: variable name
+ * @b: variable name
+ * Return: value (0)
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
