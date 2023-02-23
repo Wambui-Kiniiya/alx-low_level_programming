a1d085885c7fb544f3cf4628f0822699f0359924
Wambui-Kiniiya <wambui.kiela@gmail.com>
2023-02-23 07:28:48 -0800

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - tests whether function is +ve or -ve
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
-	int i;
+        int i;
 
-	i = 0;
-	positive_or_negative(i);
-	
-	return (0);
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
 }
