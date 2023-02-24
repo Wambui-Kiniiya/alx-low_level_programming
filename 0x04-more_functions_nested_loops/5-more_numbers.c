3e2ff3acc39fc50c457879917b930e3a990aa1c0
Wambui-Kiniiya <wambui.kiela@gmail.com>
2023-02-23 22:43:40 -0800

    Task 9

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,14 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (j = 0; j < 10; j++)
+	for (i = 0; i < 10; i++)
 	{
-		for (i = 0; i < 15; i++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (i >= 10)
+			if (j > 9)
 			{
-				_putchar((i / 10) + ('0'));
-				_putchar((i % 10) + ('0'));
+				_putchar((j / 10) + ('0'));
+				_putchar((j % 10) + ('0'));
 			}
 		}
 		_putchar('\n');
