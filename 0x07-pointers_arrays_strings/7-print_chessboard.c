806fcb61cff42b37b74a2c75d83ed8ee72f98bf2
Wambui-Kiniiya <wambui.kiela@gmail.com>
2023-03-06 10:22:14 -0800

    Task 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,9 +1,8 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * print_chessboard - prints the chessboard
+ * print_chessboard - prints chessboard
  * @a: array
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 void print_chessboard(char (*a)[8])
 {
