67440dc206e735cbf7476942b3567f4733fd63f6
Wambui-Kiniiya <wambui.kiela@gmail.com>
2023-03-08 02:43:56 -0800

    Task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,14 +6,10 @@
  */
 int factorial(int n)
 {
-	int result = n;
-
-	if (n > 0)
+	if (n < 0)
 		return (-1);
-	else if (n >= 0 && n <= 1)
+	else if (n == 0)
 		return (1);
-
-	result *= factorial(n - 1);
-
-	return (result);
+	else
+		return (n * factorial(n - 1));
 }
