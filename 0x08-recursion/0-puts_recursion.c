613e64fd64e32641013ab982b2cb26773c47cda3
Wambui-Kiniiya <wambui.kiela@gmail.com>
2023-03-08 09:55:32 -0800

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _puts_recursion - prints a string, followed by a new line.
  * @s: string
@@ -10,6 +11,7 @@ void _puts_recursion(char *s)
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+
 	else
 		_putchar('\n');
 }
